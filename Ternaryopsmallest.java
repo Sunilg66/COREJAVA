8892a3879721de5c0f8388af0792d430e73ebcb3
Sunilg66 <sunilkg251@gmail.com>
2021-05-03 12:26:45 +0530

    adding ternaryop file

---
+++ Ternaryopsmallest.java
@@ -0,0 +1,9 @@
+class Ternaryopsmallest{
+public static void main(String[] args){
+int a=250;
+int b=400;
+int result=a<b? a:b;
+System.out.println("the smallest of a&b is: ");
+System.out.println("result: "+ result);
+}
+}
