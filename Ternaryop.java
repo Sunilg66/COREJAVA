89e82cb632dd7ec37b8f1b82e9f9f7500d4f9435
Sunilg66 <sunilkg251@gmail.com>
2021-05-03 12:08:54 +0530

    adding file

---
+++ Ternaryop.java
@@ -0,0 +1,9 @@
+class Ternaryop{
+public static void main(String [] args){
+int x=100;
+int y=500;
+System.out.println(" the greatest of x&y is: ");
+int result=x>y? x:y;
+System.out.println("result: "+ result);
+}
+}
